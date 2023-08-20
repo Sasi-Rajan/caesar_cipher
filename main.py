41b775d5bf58df62e777dff1eb9656501699cc3a
Sasi-Rajan <mailbox.sasiraj@gmail.com>
2023-08-20 22:21:17 +0530

    Created caesar cipher code

---
+++ main.py
@@ -0,0 +1,52 @@
+logo = """
+ ,adPPYba, ,adPPYYba,  ,adPPYba, ,adPPYba, ,adPPYYba, 8b,dPPYba,
+a8"     "" ""     `Y8 a8P_____88 I8[    "" ""     `Y8 88P'   "Y8
+8b         ,adPPPPP88 8PP"""""""  `"Y8ba,  ,adPPPPP88 88
+"8a,   ,aa 88,    ,88 "8b,   ,aa aa    ]8I 88,    ,88 88
+ `"Ybbd8"' `"8bbdP"Y8  `"Ybbd8"' `"YbbdP"' `"8bbdP"Y8 88
+            88             88
+           ""             88
+                          88
+ ,adPPYba, 88 8b,dPPYba,  88,dPPYba,   ,adPPYba, 8b,dPPYba,
+a8"     "" 88 88P'    "8a 88P'    "8a a8P_____88 88P'   "Y8
+8b         88 88       d8 88       88 8PP""""""" 88
+"8a,   ,aa 88 88b,   ,a8" 88       88 "8b,   ,aa 88
+ `"Ybbd8"' 88 88`YbbdP"'  88       88  `"Ybbd8"' 88
+              88
+              88
+"""
+
+print(logo)
+n=1
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
+            'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p',
+            'q', 'r', 's', 't', 'u',
+            'v', 'w', 'x', 'y', 'z']
+
+
+def ceasar(ceasar_text, ceasar_shift, ceasar_direction):
+    cipher = ""
+    if ceasar_shift > 25:
+        ceasar_shift = ceasar_shift % 26
+    if ceasar_direction == "decode":
+        ceasar_shift = ceasar_shift * (-1)
+    for letter in ceasar_text:
+        if letter in alphabet:
+            position = alphabet.index(letter)
+            new_position = position + ceasar_shift
+            cipher += alphabet[new_position]
+        else:
+            cipher += letter
+    print(f"Your {ceasar_direction} message is {cipher}")
+
+while n != 2:
+    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
+    text = input("Type your message:\n").lower()
+    shift = int(input("Type the shift number:\n"))
+    ceasar(ceasar_text=text, ceasar_shift=shift, ceasar_direction=direction)
+    ans=input("Do you want to run that again ")
+    if ans == 'yes':
+        n=1
+    else:
+        n=2
+        print("Bye Bye")
